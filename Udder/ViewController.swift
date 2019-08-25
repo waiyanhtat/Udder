cf8d24890f3cf2fd8476411339f48117e8c39374
waiyan <waiyanhtat77@gmail.com>
2019-08-25 16:28:39 +0630

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Wai Yan on 8/25/19.
+//  Copyright © 2019 Wai Yan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
